f19c72dedf73d91cdf3733c39af792c5799c521a
Martins Anerua <aneruamartins@gmail.com>
2022-07-21 13:03:51 +0100

    enable persistent database connections in Heroku

--- Credity/settings.py
+++ Credity/settings.py
@@ -88,7 +88,8 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'localhost',
-        'PORT': '5432'
+        'PORT': '5432',
+        'CONN_MAX_AGE': 500
     }
 }
 
