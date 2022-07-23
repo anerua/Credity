aa5dd0918c64e384059784e6f45b03d5a084a74c
Martins Anerua <aneruamartins@gmail.com>
2022-07-23 01:20:25 +0100

    create register user view

--- account/serializers.py
+++ account/serializers.py
@@ -10,3 +10,6 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("email", "first_name", "last_name", "password")
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
